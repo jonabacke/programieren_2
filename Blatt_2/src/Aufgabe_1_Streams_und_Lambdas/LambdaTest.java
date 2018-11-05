02c3ac39e1ab8631e11e4ac0a5f23cd4a7f36acc
Jonathan Backes <jonathan.backes.jb@googlemail.com>
2018-11-05 23:49:01 +0100

    stream verbessert

--- Blatt_2/src/Aufgabe_1_Streams_und_Lambdas/LambdaTest.java
+++ Blatt_2/src/Aufgabe_1_Streams_und_Lambdas/LambdaTest.java
@@ -12,6 +12,7 @@ class LambdaTest {
 		String [] mehrWorte = {" adas "," dsfköüäösdbj f ", null , " sdßfjhbjsdh bfjkh ", " idkushgfui "};
 		_stream = new Streams_und_Lambdas();
 		assertEquals("[ADAS, DSFKOEUE, F, SDSSFJHB, BFJKH, IDKUSHGF]", _stream.streamManipulating(mehrWorte).toString());
+		assertEquals("[ADAS, DSFKOEUE, F, SDSSFJHB, BFJKH, IDKUSHGF]", _stream.manipulating(mehrWorte).toString());
 	}
 	
 	@Test
@@ -19,6 +20,7 @@ class LambdaTest {
 		String [] mehrWorte = {" äöüäö "," döäüösfköüäösdbj f ", null , " öüäöääöü bfjkh ", " ßßßßß "};
 		_stream = new Streams_und_Lambdas();
 		assertEquals("[AEOEUEAE, DOEAEUEO, F, OEUEAEOE, BFJKH, SSSSSSSS]", _stream.streamManipulating(mehrWorte).toString());
+		assertEquals("[AEOEUEAE, DOEAEUEO, F, OEUEAEOE, BFJKH, SSSSSSSS]", _stream.manipulating(mehrWorte).toString());
 	}
 	
 	@Test
@@ -26,6 +28,7 @@ class LambdaTest {
 		String [] mehrWorte = {" null ",null," asdfasdw f ", null , " wadas bfjkh ", " adsw "};
 		_stream = new Streams_und_Lambdas();
 		assertEquals("[NULL, ASDFASDW, F, WADAS, BFJKH, ADSW]", _stream.streamManipulating(mehrWorte).toString());
+		assertEquals("[NULL, ASDFASDW, F, WADAS, BFJKH, ADSW]", _stream.manipulating(mehrWorte).toString());
 	}
 
 
--- Blatt_2/src/Aufgabe_1_Streams_und_Lambdas/Streams_und_Lambdas.java
+++ Blatt_2/src/Aufgabe_1_Streams_und_Lambdas/Streams_und_Lambdas.java
@@ -2,7 +2,10 @@ package Aufgabe_1_Streams_und_Lambdas;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Streams_und_Lambdas {
 	
@@ -12,11 +15,39 @@ public class Streams_und_Lambdas {
 	public static void main(String[] args) {
 		String [] mehrWorte = {"adas","dsfköüäösdbj f",null, "sdßfjhbjsdh bfjkh", " idkushgfui"};
 		Streams_und_Lambdas stream = new Streams_und_Lambdas();
-		System.out.println(stream.streamManipulating(mehrWorte));
+		System.out.println(stream.streamManipulating(mehrWorte).toString());
+		System.out.println(stream.manipulating(mehrWorte).toString());
 
 	}
 	
-	public List<String> streamManipulating(String [] mehrWorte) {
+	public List<String> streamManipulating(String[] mehrWorte) {
+		List<String> wortListe = new ArrayList<String>();
+		for (int i = 0; i < mehrWorte.length; i++) {
+			if (mehrWorte[i] != null) {
+				wortListe.addAll(Arrays.asList(mehrWorte[i].trim().split(" ")));
+			}
+		}
+		List<String> neueWorte = new ArrayList<String>();
+		Stream<String> worte = wortListe.stream();
+		worte.forEach(str -> {
+			if (str == null) {
+				return;
+			}
+			str = str.replaceAll("ä", "ae");
+			str = str.replaceAll("ü", "ue");
+			str = str.replaceAll("ö", "oe");
+			str = str.replaceAll("ß", "ss");
+			str = str.toUpperCase();
+			str = str.trim();
+			if (str.length() > 8) {
+				str = str.substring(0, 8);
+			}
+			neueWorte.add(str);
+		});
+		return neueWorte;
+	}
+
+	public List<String> manipulating(String [] mehrWorte) {
 		_items = new ArrayList<String>();
 		_itemsFertig = new ArrayList<String>();
 		
@@ -30,7 +61,6 @@ public class Streams_und_Lambdas {
 			str = str.replaceAll("ü", "ue");
 			str = str.replaceAll("ö", "oe");
 			str = str.replaceAll("ß", "ss");
-			str = str.toUpperCase();
 			str = str.toUpperCase();
 			str = str.trim();
 			if (str.length() > 8) {
