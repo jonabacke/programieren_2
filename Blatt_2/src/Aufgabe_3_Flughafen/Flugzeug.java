d1f68e1bcbfca7fe6aa8fddb08c68f5a031487ca
Jonathan Backes <37875124+jonabacke@users.noreply.github.com>
2018-11-15 10:20:07 +0100

    abgabe

--- Blatt_2/src/Aufgabe_3_Flughafen/Flugzeug.java
+++ Blatt_2/src/Aufgabe_3_Flughafen/Flugzeug.java
@@ -1,35 +1,103 @@
 package Aufgabe_3_Flughafen;
 
-public class Flugzeug implements Runnable {
+public class Flugzeug extends Thread {
 	
-	private Flughafen _flughafen;
-	private String _id;
-	private int _flugdauer;
-	private int _startzeit;
-	private Status _status;
-	public int zeit;
+	private Status status;
+	private int flugZeit;	// Sollflugzeit
+	private int startZeit;	// Zeitpunkt des Starts
+	private int reiseZeit;	// Bisherige Reisezeit 
 	
-	public Flugzeug() {
-		// TODO Auto-generated constructor stub
+	private final String flugNr;
+	private static int nr = 0;
+	private Flughafen zielFlughafen;
+	private static final int LANDE_ZEIT = 1500; 
+
+	/**
+	 * 
+	 * @param flughafen Der Flughafen, von dem das Flugzeug agiert.
+	 * @param startZeit	Die Startzeit fr das Flugzeug (Gemessen an der Simulations Zeit)
+	 */
+	public Flugzeug(Flughafen flughafen, int startZeit) 
+	{
+		if (flughafen == null || startZeit < 0) 
+		{
+			throw new IllegalArgumentException();
+		}
+		this.startZeit = startZeit;
+		this.zielFlughafen = flughafen;
+		flugNr = "Flugnummer: " + Flugzeug.nr++;
+		status = Status.IM_FLUG;
+		flugZeit = (int) ((Math.random() * 5000) + 3000);
+		
 	}
 
 	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	public void run() 
+	{
+		getReisezeit();
+		
+		
+		System.err.println("Willkommen an Bord von " + flugNr + ". Die Maschine ist pünktlich um " + startZeit + " abgehoben.\n "
+				 + "Die voraussichtliche Flugdauer beträgt " + flugZeit + " Sekunden.");
+		
+		while (!this.isInterrupted()) 
+		{
+			
+			try {
+				Thread.sleep(500);
+			} 
+			catch (InterruptedException e) 
+			{				
+				this.interrupt();
+			}
+			getReisezeit();
+			switch (status) 
+			{
+			case IM_FLUG:
+				if (reiseZeit - startZeit >= flugZeit) {
+					if (zielFlughafen.landebahnFrei(this)) 
+					{
+						status = Status.IM_LANDEANFLUG;
+						startZeit = reiseZeit;
+						flugZeit = LANDE_ZEIT;
+						System.err.println(flugNr + " hat sein Ziel erreicht und beginnt jetzt mit dem Landeanflug.");
+					}
+				}
+
+				break;
+			case IM_LANDEANFLUG:
+					if(reiseZeit - startZeit >= flugZeit) 
+					{
+						status = Status.GELANDET;	
+						zielFlughafen.gelandet(this);
+						System.err.println(flugNr + " ist um "+ reiseZeit + " gelandet.");
+					}
+				break;
+			case GELANDET:
+				
+				break;
+				
+			}
+			
+		}
+		
+		System.err.println(flugNr + " ist wieder gelandet.");
 
 	}
+
 	
-	public void istGelandet() {
-		
+	public void getReisezeit()
+	{
+		reiseZeit = (int)System.currentTimeMillis();
 	}
 	
-	public String toString() {
-		return null;
+/**
+ * Abfrage der Flugnummer 
+ * @return	Die Flugnummer
+ */
+	public String getFlugnummer() 
+	{
+		return flugNr;
 	}
-	
-	public void setZeit() {
-		
-	}
-	
 
 }
